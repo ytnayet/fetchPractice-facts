d480c5d3929cf1fa9bbb1af6844ec017b2d2448c
mimi2709 <yetnayetkelekele@gmail.com>
2022-03-23 06:34:54 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+showFact=()=>{
+    let displayFact = document.querySelector(".display-facts")
+    let url = "https://api.chucknorris.io/jokes/random";
+fetch(url) 
+    .then(response =>{
+      return response.json()
+    })
+    .then(responseData => {
+        // console.log("response", responseData)
+        displayFact.innerHTML = responseData.value;
+    })
+}
+    
